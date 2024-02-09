657128741e9258a570808f3528508cbc588a795c
jovnc <edtokens@gmail.com>
2024-02-09 18:19:25 +0800

    fixed bug

--- src/app/admin/attendance/page.js
+++ src/app/admin/attendance/page.js
@@ -1,10 +1,13 @@
-import { getPendingApprovalEvents } from "@actions/eventActions";
+import { getAllEvents } from "@actions/eventActions";
 import EventAttendanceList from "@components/attendance/EventAttendanceList";
 import NoPending from "@components/attendance/NoPending";
 
 export default async function page() {
-  const { data: pendingEvents, error: pendingEventsError } =
-    await getPendingApprovalEvents();
+  const { data: allEvents, error: getError } = await getAllEvents();
+
+  const pendingEvents = allEvents.filter((event) => {
+    return !event.attended && event.finished;
+  });
 
   if (pendingEvents.length === 0) return <NoPending />;
 
