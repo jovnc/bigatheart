7824395a043cfd56ac31899a74ba584d4d1a7715
jovnc <edtokens@gmail.com>
2024-02-09 13:00:45 +0800

    fixed bugs

--- src/actions/certAction.js
+++ src/actions/certAction.js
@@ -115,7 +115,7 @@ export async function generateInvitation(
 ) {
   // create new PDF document to write onto using pdfkit
   const doc = new PDFDocument({
-    font: "./public/assets/fonts/butler_regular.otf",
+    font: "/assets/fonts/Butler_Regular.otf",
     layout: "landscape",
     size: "A4",
   });
@@ -123,10 +123,10 @@ export async function generateInvitation(
   // write user data into the PDF certificate
   const stream = doc.pipe(BlobStream());
 
-  doc.image("./public/assets/images/invitation.jpg", 0, 0, {
+  doc.image("/assets/images/invitation.jpg", 0, 0, {
     width: 842,
   });
-  doc.font("./public/assets/fonts/butler_regular.otf");
+  doc.font("/assets/fonts/Butler_Regular.otf");
   doc.fontSize(35).text(name, 95, 230, {});
   doc.fontSize(20).text(eventName, 95, 320, {});
   doc.fontSize(17).text(eventDate, 155, 385, {});
