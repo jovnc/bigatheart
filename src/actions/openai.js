d19b3f704d69e0dcb931e05c1c8d1966db3e809b
jovnc <edtokens@gmail.com>
2024-02-22 14:40:03 +0800

    fixed bug

--- src/actions/openai.js
+++ src/actions/openai.js
@@ -1,7 +1,7 @@
 "use server";
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export async function sendMessageToOpenAI(message) {
   let systemMessage =
